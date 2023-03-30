93b926d269662f42f425ac0b6a9017ee52083a6a
Danny-Robinson <dannyrobinson94@hotmail.com>
2023-03-30 16:48:45 +0100

    gopher

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import gopher from './assets/gopher.png';
 import './App.css';
 import Timer from './components/Timer';
 
@@ -7,7 +7,12 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+				<img
+					src={gopher}
+					className="App-logo"
+					alt="logo"
+					style={{ overflow: 'hidden', height: 200, width: 200 }}
+				/>
 				<Timer />
 			</header>
 		</div>
